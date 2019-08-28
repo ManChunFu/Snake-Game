a283879350180b383aed3fb209e304a1055da1b1
Mandy_Fu <man.chunfu@futuregames.nu>
2019-08-28 14:57:56 +0200

    snake

---
+++ Assets/Scripts/SnakeBody.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.Assertions;
+
+public class SnakeBody : MonoBehaviour
+{
+    private SnakeHead _snakeHead;
+
+    private float _speed = 1.5f;
+
+    private void Awake()
+    {
+        _snakeHead = GameObject.Find("Snake_Head").GetComponent<SnakeHead>();
+        Assert.IsNotNull(_snakeHead, "Failed to access SnakeHead script.");
+    }
+
+
+    private void Update()
+    {
+        
+
+        if (_snakeHead._isMoving)
+        {
+            MovementController();
+        }
+    }
+
+    private void MovementController()
+    {
+        float moveInput = Input.GetAxis("Horizontal");
+
+        transform.Translate(0f, moveInput * _speed * Time.deltaTime, 0f);
+    }
+}
---
+++ Assets/Scripts/SnakeHead.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class SnakeHead : MonoBehaviour
+{
+    private float _speed = 1.5f;
+    private float _rotationSensetivity = 1f;
+    public float _rotation;
+    public bool _isMoving;
+
+    private void Start()
+    {
+        _isMoving = false;
+        _rotation = 0f;
+    }
+    private void Update()
+    {
+        MovementController();
+        _isMoving = false;
+    }
+
+    private void MovementController()
+    {
+        float walkInput = Input.GetAxis("Vertical");
+        float rotationInput = Input.GetAxis("Horizontal");
+
+        transform.Translate(new Vector3(0f, -walkInput * _speed * Time.deltaTime, 0f));
+        _rotation += rotationInput * _rotationSensetivity;
+        transform.eulerAngles = new Vector3(0f, 0f, -_rotation);
+
+        _isMoving = true;
+    }
+}
